505d965ffa277f77ccc9354538f54c48feaf2111
Alexander Hill <alexanderhill4130@gmail.com>
2019-07-07 15:17:46 +0200

    updated transtions

--- assets/js/main.js
+++ assets/js/main.js
@@ -119,7 +119,7 @@
 						.scrollex({
 							top:		'30vh',
 							bottom:		'30vh',
-							delay:		50,
+							// delay:		50,
 							initialize:	function() { $(this).addClass('inactive'); },
 							terminate:	function() { $(this).removeClass('inactive'); },
 							enter:		function() { $(this).removeClass('inactive'); },
